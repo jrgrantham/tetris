763b1326442b7a949fa3475a14dbfc1e38f3bfff
James Grantham <james.r.grantham@gmail.com>
2020-04-20 13:56:15 +0100

    canvas and first shape

---
+++ tetris.js
@@ -0,0 +1,22 @@
+const canvas = document.getElementById("tetris");
+const context = canvas.getContext("2d");
+
+context.scale(20, 20)
+
+context.fillStyle = "#000";
+context.fillRect(0, 0, canvas.width, canvas.height);
+
+const matrix = [
+  [0, 0, 0],
+  [1, 1, 1],
+  [0, 1, 0],
+];
+
+matrix.forEach((row, y) => {
+  row.forEach((value, x) => {
+    if (value !== 0) {
+      context.fillStyle = "red";
+      context.fillRect(x, y, 1, 1);
+    }
+  });
+});
